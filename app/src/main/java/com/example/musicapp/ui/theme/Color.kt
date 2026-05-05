9d1d1216fcf9093d3cc3bea3134208efd01b7ab4
CarlosGallegos <gallegosgutierrezj62@gmail.com>
2026-05-05 15:41:41 -0600

    scaffolding inicial con tema morado

---
+++ app/src/main/java/com/example/musicapp/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.musicapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val PurplePrimary = Color(0xFF7C4DFF)
+val PurpleDeep = Color(0xFF5E35E0)
+val PurpleLight = Color(0xFFB39DFF)
+val PurpleBg = Color(0xFFEFE9FF)
+val PurpleScrim = Color(0xCC2D1B4E)
+val DarkPlayer = Color(0xFF1E1230)
+val CardWhite = Color(0xFFFFFFFF)
+val TextDark = Color(0xFF1B1530)
+val TextMuted = Color(0xFF6E6A85)
